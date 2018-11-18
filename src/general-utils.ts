c793ca518c79732cb3bc1a053037d12a8fda7ae6
Itamar Held <itamar.held@pyramidanalytics.com>
2018-11-18 19:22:18 +0200

    clean

--- src/general-utils.ts
+++ src/general-utils.ts
@@ -13,7 +13,4 @@ export const tail = (arr: any[]) => arr.slice(1);
 const identity = item => item;
 export const compact = (arr: any[]) => arr.filter(identity);
 
-type ReturnType<T> = T extends (...args: any[]) => infer R ? R : any;
-type Predicate = ReturnType<boolean>;
-
 export const complement = <T extends any[]>(fn: (...parmas: T) => boolean) => (...params: T) => !fn(...params);

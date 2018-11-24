1b8cf4c2480da4a7cecccf64befbb9d0ad8793cc
Itamar Held <ita@gmail.com>
2018-11-24 05:18:44 +0200

    merge square

---
+++ src/array/merge-square.ts
@@ -0,0 +1,22 @@
+import { updateInIndex } from './../utils/general-utils';
+import { RecursiveArray } from "../utils/general-types";
+
+export const mergeSquare = <T>(
+    arr1: RecursiveArray<T>,
+    arr2: RecursiveArray<T>
+): RecursiveArray<T> => {
+    function recurse(item1: RecursiveArray<T>, item2: RecursiveArray<T>) {
+        return item2.reduce(
+            (res: RecursiveArray<T>, subItem2, idx) => {
+                const subItem1 = item1[idx];
+
+                return (Array.isArray(subItem1) && Array.isArray(subItem2))
+                    ? updateInIndex(idx, recurse(subItem1, subItem2), res)
+                    : [...res, subItem2];
+            }
+            , item1
+        );
+    }
+
+    return recurse(arr1, arr2);
+};
--- src/utils/general-utils.ts
+++ src/utils/general-utils.ts
@@ -15,3 +15,38 @@ export const identity = item => item;
 export const compact = (arr: any[]) => arr.filter(identity);
 
 export const complement = <T extends any[]>(fn: (...parmas: T) => boolean) => (...params: T) => !fn(...params);
+
+export const zip = <T>(arr1: T[], arr2: T[]): [T, T][] => {
+	const minSize = Math.min(arr1.length, arr2.length);
+	return times(minSize, idx => [arr1[idx], arr2[idx]]);
+}
+
+export const updateInIndex = <T>(idx: number, newVal: T, arr: T[]): T[] => {
+	const before = arr.slice(0, Math.min(idx, arr.length - 1));
+    const after = arr.slice(Math.min(idx + 1, arr.length));
+    before
+    after
+
+	return [...before, newVal, ...after];
+}
+
+export const maxBy = <T>(fn: (item: T) => number, ...arr: T[]) => {
+	let maxVal: number;
+	let max: T;
+
+	for (let item of arr) {
+		const val = fn(item);
+		const isMax = val > maxVal;
+
+		if (isMax) {
+			maxVal = val;
+			max = item;
+		}
+	}
+
+	return max;
+};
+
+export const drop = <T>(amount: number, arr: T[]): T[] => {
+	return arr.slice(amount);
+}

964d567926079a671feacdefc1ecc40fef1ba5f8
Itamar Held <ita@gmail.com>
2018-11-27 01:42:38 +0200

    clean

--- src/array/insert-square.ts
+++ src/array/insert-square.ts
@@ -1,5 +1,5 @@
-import { changeDeep } from './../utils/general-utils';
-import { RecursiveArray } from './../utils/general-types';
+import { changeDeep } from '../utils/general-utils';
+import { RecursiveArray } from '../utils/general-types';
 
 export const insertSquare = <T>(idxs: number[], values: any[], arr: RecursiveArray<T>): RecursiveArray<T> => {
     function insert<T>(idxToUpdate: number, arrToUpdate: T[]) {

7d80a113bcbba1d2067f3a0fff7babd6c1d2a01e
Itamar Held <ita@gmail.com>
2018-11-27 01:37:55 +0200

    insert deep

---
+++ src/array/insert-deep.ts
@@ -0,0 +1,11 @@
+import { changeDeep } from './../utils/general-utils';
+import { RecursiveArray } from './../utils/general-types';
+import { update } from 'ramda';
+
+export const insertSquare = <T>(idxs: number[], values: any[], arr: RecursiveArray<T>): RecursiveArray<T> => {
+    function insert<T>(idxToUpdate: number, arrToUpdate: T[]) {
+        arrToUpdate.splice(idxToUpdate, 0, ...values);
+    }
+
+    return changeDeep(idxs, insert, arr);
+}

fe0fa5f5ad2134af3ad854917b321a211c40b40d
Itamar Held <ita@gmail.com>
2018-11-27 18:33:52 +0200

    clean

--- src/array/merge-square.ts
+++ src/array/merge-square.ts
@@ -1,11 +1,11 @@
 import { updateInIndex } from './../utils/general-utils';
-import { RecursiveArray } from "../utils/general-types";
+import { RecursiveArray, RecursiveItem } from "../utils/general-types";
 
 export const mergeSquare = <T>(
     arr1: RecursiveArray<T>,
     arr2: RecursiveArray<T>
 ): RecursiveArray<T> => {
-    function recurse(item1: RecursiveArray<T>, item2: RecursiveArray<T>) {
+    function recurse(item1: RecursiveArray<T>, item2: RecursiveArray<T>): RecursiveItem<T> {
         return item2.reduce(
             (res: RecursiveArray<T>, subItem2, idx) => {
                 const subItem1 = item1[idx];
@@ -18,5 +18,5 @@ export const mergeSquare = <T>(
         );
     }
 
-    return recurse(arr1, arr2);
+    return recurse(arr1, arr2) as RecursiveArray<T>;
 };

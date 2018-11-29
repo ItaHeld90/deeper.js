fa60709a93b2d5db80dd15889a569fdc30f0df36
Itamar Held <ita@gmail.com>
2018-11-30 01:36:47 +0200

    fix find obj deep

--- src/object/find-obj-deep.ts
+++ src/object/find-obj-deep.ts
@@ -1,9 +1,9 @@
-import { iterateObjDeep } from '../utils/iterate-obj-deep';
+import { iterateObjDeep, DeepIteratee } from '../utils/iterate-obj-deep';
 
-export const findObjDeep = <T>(predicateFn: (item: T) => boolean, path: (string | number)[], obj: Object): T => {
-	for (let { value } of iterateObjDeep(path, obj)) {
-		if (predicateFn(value)) {
-			return value;
+export const findObjDeep = <T>(predicateFn: (item: DeepIteratee) => boolean, path: (string | number)[], obj: Object): T => {
+	for (let iteratee of iterateObjDeep(path, obj)) {
+		if (predicateFn(iteratee)) {
+			return iteratee.value;
 		}
 	}
 

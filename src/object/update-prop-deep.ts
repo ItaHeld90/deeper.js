3627f530e8983fb7b1e779af0878bb6991c30569
Itamar Held <itamar.held@pyramidanalytics.com>
2018-12-25 11:53:07 +0200

    update prop deep

---
+++ src/object/update-prop-deep.ts
@@ -0,0 +1,19 @@
+import { PropsPath } from '../utils/general-types';
+import { tail } from "../utils/general-utils";
+
+export function updatePropDeep(path: PropsPath, value: any, obj: Object): Object {
+	function recurse(path: PropsPath, input: any) {
+		if (!path.length) {
+			return value;
+		}
+
+		const prop = path[0];
+
+		return {
+			...input,
+			[prop]: recurse(tail(path), input[prop]),
+		};
+	}
+
+	return recurse(path, obj);
+}
